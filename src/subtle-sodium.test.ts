98cc35145f8239f9f4ac8a18298935eb6d8ba95d
Frederick Jansen <frederick.jansen@gmail.com>
2018-06-10 00:00:03 -0400

    Implement crypto_secretbox_keygen()

--- src/subtle-sodium.test.ts
+++ src/subtle-sodium.test.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:no-console */
 import { expect } from 'chai';
 import { SubtleSodium as subtle } from '../src/subtle-sodium';
 
@@ -7,6 +8,20 @@ it('randombytes_buf return Uint8Array of correct length', () => {
     expect(ar).to.be.a('Uint8Array');
 });
 
-it('crypto_secretbox_KEYBYTES is 32', () => {
-    expect(subtle.crypto_secretbox_KEYBYTES).to.be.equal(32);
+describe('Secret-key authenticated encryption', () => {
+    it('should generate key', (done) => {
+        subtle.crypto_secretbox_keygen().then((key) => {
+            expect(key).to.be.a('CryptoKey');
+            expect(key.extractable).to.equal(true);
+            done();
+        });
+    });
+
+    it('crypto_secretbox_KEYBYTES is 32', () => {
+        expect(subtle.crypto_secretbox_KEYBYTES).to.be.equal(32);
+    });
+
+    it('crypto_secretbox_NONCEBYTES is 32', () => {
+        expect(subtle.crypto_secretbox_NONCEBYTES).to.be.equal(12);
+    });
 });
--- src/subtle-sodium.ts
+++ src/subtle-sodium.ts
@@ -6,14 +6,20 @@ export class SubtleSodium {
         return window.crypto.getRandomValues(new Uint8Array(length));
     }
 
-    public static async crypto_secretbox_easy(message: string, nonce: Uint8Array, key) {
+    public static async crypto_secretbox_keygen() {
+        const alg = {name: 'AES-GCM', length: SubtleSodium.crypto_secretbox_KEYBYTES * 8};
+
+        return await window.crypto.subtle.generateKey(alg, true, ['encrypt', 'decrypt', 'wrapKey', 'unwrapKey']);
+    }
+
+    public static async crypto_secretbox_easy(message: string, nonce: Uint8Array, key: CryptoKey) {
         const msUtf8 = new TextEncoder().encode(message);
         const alg = {name: 'AES-GCM', iv: nonce, tagLength: 128};
 
         return await window.crypto.subtle.encrypt(alg, key, msUtf8);
     }
 
-    public static async crypto_secretbox_easy_open(ciphertext: string, nonce: Uint8Array, key) {
+    public static async crypto_secretbox_easy_open(ciphertext: string, nonce: Uint8Array, key: CryptoKey) {
         const ctUtf8 = new TextEncoder().encode(ciphertext);
         const alg = {name: 'AES-GCM', iv: nonce, tagLength: 128};
 
