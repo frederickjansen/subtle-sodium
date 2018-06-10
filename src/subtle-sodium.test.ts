c8f9fb9e605af71bfb0b75e1425ede6070a81537
Frederick Jansen <frederick.jansen@gmail.com>
2018-06-09 23:02:02 -0400

    Test randombytes_buf

--- src/subtle-sodium.test.ts
+++ src/subtle-sodium.test.ts
@@ -1,8 +1,12 @@
 import { expect } from 'chai';
 import { SubtleSodium as subtle } from '../src/subtle-sodium';
 
-describe('HelloComponent', () => {
-    it('crypto_secretbox_KEYBYTES is 32', () => {
-        expect(subtle.crypto_secretbox_KEYBYTES).to.be.equal(32);
-    });
+it('randombytes_buf return Uint8Array of correct length', () => {
+    const ar = subtle.randombytes_buf(10);
+    expect(ar).to.have.lengthOf(10);
+    expect(ar).to.be.a('Uint8Array');
+});
+
+it('crypto_secretbox_KEYBYTES is 32', () => {
+    expect(subtle.crypto_secretbox_KEYBYTES).to.be.equal(32);
 });
